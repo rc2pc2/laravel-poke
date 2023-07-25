bf59f3d861e40e53946542ee3784b58361d0e252
RCPC <petriccarcc@gmail.com>
2023-07-25 10:27:20 +0200

    modified gym column on trainers table and added up doctrine/dbal

---
+++ database/migrations/2023_07_25_082357_update_gym_column_to_trainers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trainers', function (Blueprint $table) {
+            // ? passare da vuoto a 100 caratteri di varchar
+            $table->string('gym', 100)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trainers', function (Blueprint $table) {
+            // ? passare da 100 a vuoto caratteri di varchar
+            $table->string('gym')->change();
+        });
+    }
+};
